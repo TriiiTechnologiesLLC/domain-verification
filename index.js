a4b91320923213ebc3a554c153c4a592054b6fb4
gmjacob <george.mammenj@gmail.com>
2018-07-09 11:46:54 +0530

    made fixes suggested by vinay

--- index.js
+++ index.js
@@ -5,83 +5,81 @@ var request = require('request');
 var Promise = require("bluebird");
 var dns = require("dns");
 
-var DomainVerifaction = (function() {
+var DomainVerification = (function() {
 	
   
 	var htmlVerification = function(domain_url,domain_value,domain_html_name,hash_value) {
-		var originalArgs = arguments;
+		var original_args = arguments;
 		return new Promise(function(resolve,reject){
-			if(originalArgs.length == 4)
-		{
-			var url = domain_url+'/'+domain_html_name+'-'+domain_value+'.html';
-			var options = {
-				method: 'GET',
-				url: url
-			}
-			request(options,function(error,response,body){
-				if(error)
-				{
-					resolve(false);
-				} else {
-
-					if(parseInt(hash_value) == parseInt(body))
-					{
-						resolve(true);
-					} else {
-						resolve(false);
-					}
+			if(original_args.length == 4)
+			{
+				var url = domain_url+'/'+domain_html_name+'-'+domain_value+'.html';
+				var options = {
+					method: 'GET',
+					url: url
 				}
-			});
-		} else {
-			reject('Mismatch arguments');
-		}
+				request(options,function(error,response,body){
+					if(error)
+					{
+						resolve(false);
+					} else {
+
+						if(hash_value == body)
+						{
+							resolve(true);
+						} else {
+							resolve(false);
+						}
+					}
+				});
+			} else {
+				reject('Mismatch arguments');
+			}
 		});
 	}
   
 	var txtVerification = function(domain_url,domain_key,domain_value) {
-		var originalArgs = arguments;
+		var original_args = arguments;
 		return new Promise(function (resolve,reject){
-		if(originalArgs.length == 3){
-			dns.resolveTxt(domain_url, function(error,records){
-				if(records == undefined)
-				{
-					resolve(false);
-				} else {
-					records.forEach(function(record){
-						var expected = domain_key+'='+domain_value;
-						if(expected == record[0])
-						{
-							resolve(true);
-						}
-						else {
-							resolve(false);
-						}
-							
-					});
-				}
-				
-			});
-		}
-		else {
-			reject('Mismatch arguments')
-		}
+			if(original_args.length == 3){
+				dns.resolveTxt(domain_url, function(error,records){
+					if(records == undefined)
+					{
+						resolve(false);
+					} else {
+						records.forEach(function(record){
+							var expected = domain_key+'='+domain_value;
+							if(expected == record[0])
+							{
+								resolve(true);
+							}
+							else {
+								resolve(false);
+							}				
+						});
+					}
+				});
+			}
+			else {
+				reject('Mismatch arguments')
+			}
 	  });
 	}
   
 	var metaTagVerification = function(domain_url,domain_key,domain_value) {
-		var originalArgs = arguments;
+		var original_args = arguments;
 		return new Promise(function(resolve,reject){
-			if(originalArgs.length == 3)
+			if(original_args.length == 3)
 			{
 				metafetch.fetch(domain_url,function(err,result){
 					if(err)
 					{
 						resolve(false);
 					} else {
-						var checkKey = result.meta[domain_key];
-						if( checkKey != undefined)
+						var check_key = result.meta[domain_key];
+						if( check_key != undefined)
 						{
-							if(checkKey === domain_value)
+							if(check_key === domain_value)
 							{
 								resolve(true);
 							} else {
@@ -90,8 +88,6 @@ var DomainVerifaction = (function() {
 						} else {
 							resolve(false);
 						}
-						
-						
 					}
 				});
 			} else {
@@ -108,4 +104,4 @@ var DomainVerifaction = (function() {
 
   })();
 
-  module.exports = DomainVerifaction;
+  module.exports = DomainVerification;
