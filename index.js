bb3c661cc4090e30cee0fc7dee3cfe3e633a95ba
triii <chris@triii.org>
2025-09-10 18:47:41 -0400

    modernize add axios versus request

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
-"use strict";
+const metafetch = require('metafetch');
+const dns = require("dns");
+const parser = require('getdomain');
+const axios = require('axios');
 
-var metafetch = require('metafetch');
-var request = require('request');
-var Promise = require("bluebird");
-var dns = require("dns");
-var parser = require('getdomain');
+const DomainVerification = (function() {
+	let htmlVerification = function (domain_url, domain_html_name, hash_value) {
+		const original_args = arguments;
+		return new Promise(function (resolve, reject) {
+			if (original_args.length === 3) {
 
-var DomainVerification = (function() {
-	
-  
-	var htmlVerification = function(domain_url,domain_html_name,hash_value) {
-		var original_args = arguments;
-		return new Promise(function(resolve,reject){
-			if(original_args.length == 3)
-			{
-				var obj = {};
+				const obj = {};
 				obj.verified = "htmlVerification";
-				var url = domain_url+'/'+domain_html_name+'.html';
-				var options = {
-					method: 'GET',
-					url: url
-				}
-				request(options,function(error,response,body){
-					if(error)
-					{
+				const url = domain_url + '/' + domain_html_name + '.html';
+
+				axios.get(url)
+					.then(response => {
+						if (hash_value === response.data) {
+							obj.status = true;
+						} else {
+							obj.status = false;
+						}
+						resolve(obj);
+					})
+					.catch(error => {
+						obj.status = false;
+						resolve(obj);
+					});
+			} else {
+				reject('Mismatch arguments');
+			}
+		});
+	};
+
+	const txtVerification = function (domain_url, domain_key, domain_value) {
+		const original_args = arguments;
+		return new Promise(function (resolve, reject) {
+			if (original_args.length === 3) {
+				const obj = {};
+				obj.verified = "txtVerification";
+				const domain = parser.hostname(domain_url);
+				dns.resolveTxt(domain, function (error, records) {
+					if (records === undefined) {
 						obj.status = false;
 						resolve(obj);
 					} else {
-						if(hash_value == body)
-						{
+						const success = [];
+
+						let expected = domain_key + '=' + domain_value;
+
+						if(domain_key === "@") {
+							expected = domain_value;
+						}
+
+						records.forEach(function (record) {
+							if (expected === record[0]) {
+								success.push(obj.status);
+							}
+						});
+
+						if (success.length > 0) {
 							obj.status = true;
 							resolve(obj);
 						} else {
@@ -38,69 +68,26 @@ var DomainVerification = (function() {
 					}
 				});
 			} else {
-				reject('Mismatch arguments');
-			}
-		});
-	}
-  
-	var txtVerification = function(domain_url,domain_key,domain_value) {
-		var original_args = arguments;
-		return new Promise(function (resolve,reject){
-			if(original_args.length == 3){
-				var obj = {};
-				obj.verified = "txtVerification";
-				var domain = parser.hostname(domain_url);
-				dns.resolveTxt(domain, function(error,records){
-					if(records == undefined)
-					{
-						obj.status = false;
-						resolve(obj);
-					} else {
-						var success = [];
-						var expected = domain_key+'='+domain_value;
-						records.forEach(function(record){
-							if(expected == record[0])
-							{
-								success.push(obj.status);
-							} 				
-						});
-
-						if(success.length > 0)
-						{
-							obj.status = true;
-							resolve(obj);
-						} else {
-							obj.status = false;
-							resolve(obj);
-						}
-					}
-				});
-			}
-			else {
 				reject('Mismatch arguments')
 			}
-	  });
-	}
-  
-	var metaTagVerification = function(domain_url,domain_key,domain_value) {
-		var original_args = arguments;
-		return new Promise(function(resolve,reject){
-			var obj = {};
-				obj.verified = "metaTagVerification";
+		});
+	};
 
-			if(original_args.length == 3)
-			{
-				metafetch.fetch(domain_url,function(err,result){
-					if(err)
-					{
+	const metaTagVerification = function (domain_url, domain_key, domain_value) {
+		const original_args = arguments;
+		return new Promise(function (resolve, reject) {
+			const obj = {};
+			obj.verified = "metaTagVerification";
+
+			if (original_args.length === 3) {
+				metafetch.fetch(domain_url, function (err, result) {
+					if (err) {
 						obj.status = false;
 						resolve(obj);
 					} else {
-						var check_key = result.meta[domain_key];
-						if( check_key != undefined)
-						{
-							if(check_key === domain_value)
-							{
+						const check_key = result.meta[domain_key];
+						if (check_key !== undefined) {
+							if (check_key === domain_value) {
 								obj.status = true;
 								resolve(obj);
 							} else {
@@ -119,30 +106,26 @@ var DomainVerification = (function() {
 		});
 	};
 
-	var verifyAll = function(domain_url,domain_key,domain_value,domain_html_name,hash_value)
-	{
-		var original_args = arguments;
-		return new Promise(function(resolve,reject){
-			var obj = {};
-			var success = [];
-			var failure = [];
-			if(original_args.length == 5)
-			{
+	const verifyAll = function (domain_url, domain_key, domain_value, domain_html_name, hash_value) {
+		const original_args = arguments;
+		return new Promise(function (resolve, reject) {
+			const obj = {};
+			const success = [];
+			const failure = [];
+			if (original_args.length === 5) {
 				Promise.all([
-					htmlVerification(domain_url,domain_html_name,hash_value),
-					txtVerification(domain_url,domain_key,domain_value),
-					metaTagVerification(domain_url,domain_key,domain_value)
-				]).then(results_data =>{
-					results_data.forEach( data=>{
-						if(data.status)
-						{
+					htmlVerification(domain_url, domain_html_name, hash_value),
+					txtVerification(domain_url, domain_key, domain_value),
+					metaTagVerification(domain_url, domain_key, domain_value)
+				]).then(results_data => {
+					results_data.forEach(data => {
+						if (data.status) {
 							success.push(data.verified);
 						} else {
 							failure.push(data.verified);
 						}
 					});
-					if(success.length > 0)
-					{
+					if (success.length > 0) {
 						obj.status = true;
 						obj.verified = success;
 					} else {
@@ -150,7 +133,7 @@ var DomainVerification = (function() {
 						obj.verified = [];
 					}
 					resolve(obj);
-				}).catch( error =>{
+				}).catch(error => {
 					reject(error);
 				});
 			} else {
@@ -159,7 +142,7 @@ var DomainVerification = (function() {
 		});
 	};
 
-  
+
 	return {
 		html: htmlVerification,
 		txt: txtVerification,
